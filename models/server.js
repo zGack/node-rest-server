48b798809177f310bbcaa79647e01174d30d2941
zGack <smf-mena@hotmail.com>
2022-12-04 09:30:39 -0500

    Default server port added

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ export class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 8080;
     this.usersPath = '/api/users';
 
     // Middlewares
